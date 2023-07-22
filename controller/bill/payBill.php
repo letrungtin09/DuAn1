77990babb315c8d1cd78e45b98a3e5eb8ccbcc20
tuongduy <duytuong18897@gmail.com>
2023-07-22 23:06:51 +0700

    Hoàn thiện thanh toán và thêm học sinh vào bảng student_course

--- controller/bill/payBill.php
+++ controller/bill/payBill.php
@@ -1,6 +1,12 @@
 <?php
+require_once 'model/student_course.php';
 // Chuyển đổi thành mua hàng trong bảng bill
 bill_update_status_bill($_GET['paybill']);
-
+// Lấy khóa học trong bill
+$courseInBill = detailBill_select_by_idBill($_GET['paybill']);
+// thêm sinh viên vào bảng student_course
+foreach($courseInBill as $courseAdd){
+    student_course_insert($_SESSION['user']['userId'], $courseAdd['courseId']);
+}
 ?>
 <meta http-equiv="refresh" content="0;url=<?=$base_url?>">
